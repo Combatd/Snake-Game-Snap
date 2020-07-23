150c112cac10dec61bde3da232d6d06d10f11d93
Combatd <combatdstar@gmail.com>
2020-07-23 11:40:14 -0700

    Create GET request for '/ route

--- server.js
+++ server.js
@@ -2,6 +2,12 @@ const express = require('express');
 const app = express();
 const PORT = 8000;
 
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', (req, res) => {
+    res.sendFile('index.html');
+});
+
 app.listen(PORT, () => {
     console.log(`Snake-Game-Snap server listening on PORT ${PORT}`);
 });
