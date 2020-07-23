b1c7e256cd0611f65d5f0423c96876f48e72284b
Combatd <combatdstar@gmail.com>
2020-07-23 11:50:41 -0700

    Send index.html as static file

--- server.js
+++ server.js
@@ -2,9 +2,11 @@ const express = require('express');
 const app = express();
 const PORT = 8000;
 
+// app.use(express.static('public))
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', (req, res) => {
+    // res.sendFile(path.join(__dirname, '/public', index.html))
     res.sendFile('index.html');
 });
 
