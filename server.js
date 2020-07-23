edea5732c72cf71842516a54757b8801dadee3d4
Combatd <combatdstar@gmail.com>
2020-07-23 11:02:42 -0700

    Define server.js listener

--- server.js
+++ server.js
@@ -3,5 +3,5 @@ const app = express();
 const PORT = 8000;
 
 app.listen(PORT, () => {
-    console.log(`Snake-Game-Snap server listening on PORT ${PORT}`)l
+    console.log(`Snake-Game-Snap server listening on PORT ${PORT}`);
 });
